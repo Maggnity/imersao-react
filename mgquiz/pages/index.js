0a624384d45bae84102318f872532ee4fa6a4821
Fernando <mag.nando07@gmail.com>
2022-03-11 09:56:49 -0300

    Criação ds primeiros components e aplicação de estilização

--- mgquiz/pages/index.js
+++ mgquiz/pages/index.js
@@ -1,10 +1,53 @@
-import styled from 'styled-components'
+import React from 'react';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import styled from 'styled-components'
+import db from '../db.json';
+import { Widget , WidgetContent, WidgetHeader } from '../src/components/Widget';
+import { Footer } from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+//import QuizLogo from '../src/components/QuizLogo';
+
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto; 
+    padding: 15px;
+  }
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
-}
+  return (
+    
+     <QuizBackground BackgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <WidgetHeader>
+            <h1>
+            {db.title}
+            </h1>
+          </WidgetHeader>
+          <WidgetContent>
+            <p>{db.description}</p>
+          </WidgetContent>
+
+        </Widget>
+        <Widget>
+          <WidgetHeader>
+            <h1>Nota 2</h1>
+          </WidgetHeader>
+          <WidgetContent>
+            <p>Lorem</p>
+          </WidgetContent>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl={"https://github.com/Maggnity"}/>
+    </QuizBackground>    
+  )
+};
